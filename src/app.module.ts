5d84e39ec357fc98f5cfc5eaa2d8284982be8226
Geovane <geovanegallotticazal@hotmail.com>
2023-10-01 12:34:56 -0300

    Feat/initial adjustments (#1)
    * feat[wip]: initial adjustments
    
    * feat: create main.ts initial configs
    
    * chore: move prisma module to modules folder
    
    * feat: add github workflow and pr template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from './modules/config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from './modules/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    credentials: true,
+    optionsSuccessStatus: HttpStatus.OK,
+    origin: '*',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  configService.configureSwagger(app);
+
+  const serverPort = Number(process.env.SERVER_PORT) || 3000;
+
+  await app.listen(serverPort, '0.0.0.0', () => {
+    Logger.log(`Server running on port ${serverPort}`, 'Bootstrap');
+  });
 }
 bootstrap();
