be78e7ca5864034bc27b548090ffd719cccdc025
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2023-10-02 21:32:53 -0300

    fix: app module and user module imports

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, UserModule],
   controllers: [],
   providers: [],
+  exports: [],
 })
 export class AppModule {}
