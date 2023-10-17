b39bebbba9c479069391b14e539b35fd6a75fd85
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2023-10-17 17:47:03 -0300

    fix: add schedule controller to schedules module

--- src/modules/teacher/submodules/schedules/schedules.module.ts
+++ src/modules/teacher/submodules/schedules/schedules.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { ScheduleRepository } from './infra/repositories/schedule.repository';
 import { ScheduleService } from './services/schedule.service';
+import { ScheduleController } from './infra/controllers/schedule.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [],
+  controllers: [ScheduleController],
   providers: [ScheduleService, ScheduleRepository],
   exports: [ScheduleService, ScheduleRepository],
 })
